6df7067e07adf4e5f491b66bf5dcc2d0eadf0228
xinran.lxr <xinran.lxr@alibaba-inc.com>
2017-02-16 17:39:56 +0800

    init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "no-unused-vars": [
+            1, 
+            {
+                "vars": "all", "args": "after-used"
+            }
+        ]
+    }
+};

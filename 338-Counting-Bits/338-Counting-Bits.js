880f6d00e1d0aee1b17d2e5adf012ffa36ed58e3
xinran.lxr <xinran.lxr@alibaba-inc.com>
2017-04-18 16:35:08 +0800

    【338】Your runtime beats 23.21% of javascript submissions.

---
+++ 338-Counting-Bits/338-Counting-Bits.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/counting-bits/
+
+/**********************************************************************************
+ *
+ * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+ * 
+ * Example:
+ *    For num = 5 you should return [0,1,1,2,1,2].
+ * 
+ * Follow up:
+ *    It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+ *    Space complexity should be O(n).
+ *    Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+ * 
+ **********************************************************************************/
+
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function(num) {
+  const res = []
+  for(let i = 0; i <= num; i++) {
+    res[i] = i.toString(2).replace(/0/g, '').length
+  }
+  return res
+};
+
+// countBits(5) // [0,1,1,2,1,2]

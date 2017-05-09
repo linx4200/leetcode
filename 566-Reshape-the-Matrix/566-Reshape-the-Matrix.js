1881f634dedf21d445adce039cbe9cd103bedabe
liuxinran <liuxinran1008@gmail.com>
2017-05-10 07:35:06 +0800

    【566】Your runtime beats 39.26 % of javascript submissions.

---
+++ 566-Reshape-the-Matrix/566-Reshape-the-Matrix.js
@@ -0,0 +1,66 @@
+// Source : https://leetcode.com/problems/reshape-the-matrix/
+
+/**********************************************************************************
+ *
+ * In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original
+ * data.
+ * 
+ * You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column
+ * number of the wanted reshaped matrix, respectively.
+ * 
+ * The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+ * 
+ * If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+ * 
+ * Example 1:
+ *   Input:
+ *   nums = 
+ *   [[1,2],
+ *    [3,4]]
+ *    r = 1, c = 4
+ *   Output: 
+ *   [[1,2,3,4]]
+ *   Explanation:
+ *   The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+ * 
+ * Example 2:
+ *   Input: 
+ *   nums = 
+ *   [[1,2],
+ *    [3,4]]
+ *   r = 2, c = 4
+ *   Output: 
+ *   [[1,2],
+ *    [3,4]]
+ *   Explanation:
+ *   There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+ * 
+ * Note:
+ *   The height and width of the given matrix is in range [1, 100].
+ *   The given r and c are all positive.
+ * 
+ **********************************************************************************/
+
+/**
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+const matrixReshape = (nums, r, c) => {
+  const flat = nums.reduce((prev, cur) => {
+    return prev.concat(cur)
+  }, []);
+  if (r * c !== flat.length) return nums;
+
+  const res = [];
+  for(let i = 0; i < r; i++) {
+    res[i] = flat.slice(i * c, i * c + c)
+  }
+  return res;
+};
+
+// matrixReshape([[1,2,3], [4,5,6]], 2, 3);
+// matrixReshape([[1,2,3], [4,5,6]], 3, 2);
+// matrixReshape([[1,2], [4,5]], 1, 4);
+// matrixReshape([[1,2], [4,5]], 1, 3);
